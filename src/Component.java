b75ff786494167f6213d372259e6447f223365ea
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-13 18:54:04 +0200

    Update Component.java

--- src/Component.java
+++ src/Component.java
@@ -5,18 +5,30 @@ public abstract  class Component {
   protected int durationTime;
   protected Date initialDate;
   protected Date finalDate;
+  protected Component father;
 
   public String getName() { return this.name; }
-  public int getWastedTime() { return durationTime; }
-  public Date getInitialDate() { return initialDate; }
-  public Date getFinalDate() { return finalDate; }
+  public int getWastedTime() { return this.durationTime; }
+  public Date getInitialDate() {
+    updateDates();
+    return this.initialDate;
+  }
+  public Date getFinalDate() {
+    updateDates();
+    return this.finalDate;
+  }
 
   public abstract void updateDates();
 
-  public Component(String name) {
+  public Component(String name, Component father) {
     this.name = name;
     initialDate = null;
     finalDate = null;
     durationTime = 0;
+    this.father = father;
+  }
+
+  public Component getFather() {
+    return this.father;
   }
 }
