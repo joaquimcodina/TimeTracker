aedfe223d069f4ce0130b12f68719e2254958fc3
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-13 18:51:56 +0200

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -5,11 +5,16 @@ public class Project extends Component {
   private ArrayList<Component> Components;
   //Components: This ArrayList will contain the sub-Tasks and Sub-Projects of a Project object.
 
-  public Project(String name) {
-    super(name);
+  public Project(String name, Project father) {
+    super(name, father);
     Components = new ArrayList<Component>();
+    try {
+      father.addComponent(this);
+    }catch(NullPointerException e) { }
   }
   //Constructor: This initiates the Project with a name and sets the Components attribute to NOT NULL.
+  //NOTE: father may be a Component, but as it is always going to be a Project (Only a Project can have sub-Tasks&sub-Project) we forced the parameter to be a Project.
+  // the try-catch statement is that in case of having a null father (Project or Task Root) control the Exception.
 
   @Override
   public void updateDates() {
