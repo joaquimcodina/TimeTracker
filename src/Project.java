b0d6b77e84ee715c98c0d716318696b034a15932
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-19 14:00:35 +0200

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -18,35 +18,18 @@ public class Project extends Component {
 
   @Override
   public void updateDates() {
-    if (!Components.isEmpty()) {
-      for (int i = 0; i < Components.size(); i++)
-        Components.get(i).updateDates();
-      Date initialDate = null;
-      Date finalDate = null;
-      for (int i = 0; i < Components.size(); i++) {
-        if (initialDate == null) {
-          if (Components.get(i).getInitialDate() != null)
-            initialDate = Components.get(i).getInitialDate();
-        }
-        else if (initialDate.compareTo(Components.get(i).getInitialDate()) > 0)
-          initialDate = Components.get(i).getInitialDate();
-
-        if (finalDate == null) {
-          if (Components.get(i).getFinalDate() != null)
-            finalDate = Components.get(i).getFinalDate();
-        }
-        else if (finalDate.compareTo(Components.get(i).getFinalDate()) < 0)
-          finalDate = Components.get(i).getFinalDate();
-      }
-      this.finalDate = finalDate;
-      this.initialDate = initialDate;
+    int duration = 0;
+    for (int i = 0; i < Components.size(); i++) {
+      Components.get(i).updateDates();
+      duration += Components.get(i).getWastedTime();
     }
+    notifyFather();
+    this.durationTime = duration;
   }
   //This function updates the initialDate and finalDate of a project, including the same attributes from each of its sub-Project and sub-Tasks.
+  // It also updates its durationTime attribute and notifies its father about this change, if exists.
 
   public ArrayList<Component> getSubComponents() { return this.Components; }
 
   public void addComponent(Component obj) { Components.add(obj); } //check if it does not already exists
-
-  public void removeComponent(Component obj) { Components.remove(obj); } //check if it does not already exists
 }
