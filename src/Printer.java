667ec7879f57252d486ba3e18170421c2d0b5490
AleixCaasas <70470419+aleixcaasas@users.noreply.github.com>
2022-10-27 18:17:33 +0200

    Update Printer.java

--- src/Printer.java
+++ src/Printer.java
@@ -1,20 +1,40 @@
-public class Printer implements Visitor{
-  private Project project;
-  public Printer(Project project){
-    this.project = project;
-  }
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-  @Override
-  public void visitProject(Project project) {
-    System.out.println("Project " + project);
-    for(Component component : project.getComponentList())
-      component.accept(this);
-  }
+import java.util.Iterator;
 
-  @Override
-  public void visitTask(Task task){
-    System.out.println("Task " + task);
-    if(task.getInterval()!=null)
-      System.out.println(task.getInterval());
-  }
+public class Printer implements Visitor {
+    private Project project;
+
+    public Printer(Project project) {
+        this.project = project;
+    }
+
+    public void visitProject(Project project) {
+        System.out.println("Project " + project);
+        Iterator var2 = project.getComponentList().iterator();
+
+        while(var2.hasNext()) {
+            Component component = (Component)var2.next();
+            component.accept(this);
+        }
+
+    }
+
+    public void visitTask(Task task) {
+        System.out.println("Task " + task);
+        if (task.getIntervals() != null) {
+            System.out.println(task.getIntervals());
+        }
+
+    }
+
+    public void visitInterval(Interval interval) {
+        if (interval != null) {
+            System.out.println("Interval " + interval);
+            System.out.println(interval);
+        }
+    }
 }
