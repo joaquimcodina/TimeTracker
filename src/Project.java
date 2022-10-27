50ec0d7ebcdf96c03a45affd390966f2442dff8a
AleixCaasas <70470419+aleixcaasas@users.noreply.github.com>
2022-10-27 18:18:11 +0200

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -1,34 +1,34 @@
 import java.util.ArrayList;
 
 public class Project extends Component {
-  private ArrayList<Component> componentList = new ArrayList<>();
+    private ArrayList<Component> componentList = new ArrayList();
 
-  Project(String name, Project father) {
-    super(name, father);
-    father.addProject(this);
-  }
-
-  Project(String name) {
-    super(name);
-  }
-
-  protected void addProject(Project project) { this.componentList.add(project);
-  }
-
-  protected void addTask(Task task) { this.componentList.add(task);
-  }
-
-  public ArrayList<Component> getComponentList() { return componentList; }
-
-  public void accept(Visitor v) { v.visitProject(this); }
-
-  @Override
-  public String toString() {
-    if(getFather()!=null){
-      return getName() + "       child of " + getFather().getName() + "    " + getStartDate() + "       " + getFinalDate() + "      " + getElapsedTime().getSeconds();
+    Project(String name, Project father) {
+        super(name, father);
+        father.addProject(this);
     }
-    else{
-      return getName() + "     child of null" + "    " + getStartDate() + "       " + getFinalDate() + "      " + getElapsedTime().getSeconds();
+
+    Project(String name) {
+        super(name);
     }
-  }
+
+    protected void addProject(Project project) {
+        this.componentList.add(project);
+    }
+
+    protected void addTask(Task task) {
+        this.componentList.add(task);
+    }
+
+    public ArrayList<Component> getComponentList() {
+        return this.componentList;
+    }
+
+    public void accept(Visitor v) {
+        v.visitProject(this);
+    }
+
+    public String toString() {
+        return this.getFather() != null ? this.getName() + "       child of " + this.getFather().getName() + "    " + this.getStartDate() + "       " + this.getFinalDate() + "      " + this.getElapsedTime().getSeconds() : this.getName() + "     child of null    " + this.getStartDate() + "       " + this.getFinalDate() + "      " + this.getElapsedTime().getSeconds();
+    }
 }
