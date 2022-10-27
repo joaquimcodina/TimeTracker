1027b461976c9c2602dc8b706a68a7548b4fb2cc
AleixCaasas <70470419+aleixcaasas@users.noreply.github.com>
2022-10-27 18:18:36 +0200

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,42 +1,43 @@
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Task extends Component {
-  private Interval interval;
+    private List<Interval> intervals = new LinkedList();
 
-  public Task(String name, Project father){
-    super(name, father);
-    father.addTask(this);
-  }
-
-  public void start(LocalDateTime now) {
-    this.interval = new Interval(now, this);
-    ClockTimer.getInstance().addObserver(this.interval); //Afegim l'observer
-    // Comprobem si tenen una data d'inici existent per a no sobreescriure-la
-    if (this.getStartDate() == null) {
-      //this.setStartDate(LocalDateTime.now());
-      this.setStartDate(now);
+    public Task(String name, Project father) {
+        super(name, father);
+        father.addTask(this);
     }
-    if (this.getFather().getStartDate() == null) {
-      //this.getFather().setStartDate(LocalDateTime.now());
-      this.getFather().setStartDate(now);
+
+    public void start(LocalDateTime now) {
+        Interval interval = new Interval(now, this);
+        ClockTimer.getInstance().addObserver(interval);
+        if (this.getStartDate() == null) {
+            this.setStartDate(now);
+        }
+
+        if (this.getFather().getStartDate() == null) {
+            this.getFather().setStartDate(now);
+        }
+
+        this.intervals.add(interval);
     }
-  }
 
-  //Aquest métode para la tasca quan "l'usuari" ho demana
-  public void stop() {
-    this.setFinalDate(LocalDateTime.now());
-    this.getFather().setFinalDate(LocalDateTime.now());
-    //this.interval.stopClock();
-  }
+    public void stop() {
+        this.setFinalDate(LocalDateTime.now());
+        this.getFather().setFinalDate(LocalDateTime.now());
+    }
 
-  public void accept(Visitor v){
-    v.visitTask(this);
-  }
+    public void accept(Visitor v) {
+        v.visitTask(this);
+    }
 
-  public Interval getInterval() { return interval; }
+    public List<Interval> getIntervals() {
+        return this.intervals;
+    }
 
-  @Override
-  public String toString() {
-    return getName() + "          child of " + getFather().getName() + "      " +getStartDate() + "       " + getFinalDate() + "     " + getElapsedTime().getSeconds();
-  }
+    public String toString() {
+        return this.getName() + "          child of " + this.getFather().getName() + "      " + this.getStartDate() + "       " + this.getFinalDate() + "     " + this.getElapsedTime().getSeconds();
+    }
 }
