3cf7c8c1bc65d154e5d4ddc7251b7b9e0d3949ad
AleixCaasas <70470419+aleixcaasas@users.noreply.github.com>
2022-10-27 18:19:28 +0200

    Update Visitor.java

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,4 +1,7 @@
 public interface Visitor {
-  void visitProject(Project project);
-  void visitTask(Task task);
+    void visitProject(Project var1);
+
+    void visitTask(Task var1);
+
+    void visitInterval(Interval var1);
 }
