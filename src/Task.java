41ea2a690ece7e79a5f3c3de890bc60707848a3c
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-31 21:30:02 +0100

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -4,69 +4,68 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Task extends Component {
-    private List<Interval> intervals = new LinkedList();
+  private List<Interval> intervals = new LinkedList();
 
-    public Task(String name, Project father) {
-        super(name, father);
-        father.addTask(this);
+  public Task(String name, Project father) {
+    super(name, father);
+    father.addComponent(this);
+  }
+
+  public void start() {
+    Interval interval = new Interval(this);
+    ClockTimer.getInstance().addObserver(interval);
+
+    this.setStartDate(interval.getStart());
+
+    this.intervals.add(interval);
+  }
+  //This method Creates a new Interval and stores it into the this.intervals attribute, and,
+  // also, it updates the startDate of every father it has through the setStartDate method, if needed.
+
+  private void stopIntervals() {
+    for (Interval interval : this.intervals) {
+      if (interval.getEnd() == null)
+        interval.update();
     }
+  }
 
-    public void start(LocalDateTime now) {
-        Interval interval = new Interval(now, this);
-        ClockTimer.getInstance().addObserver(interval);
-        if (this.getStartDate() == null) {
-            this.setStartDate(now);
-        }
+  public void stop() {
+    stopIntervals();
+    this.updateDates();
+  }
+  //This method stops every interval of a task and updates itself's finalDate and elapsedTime.
 
-        if (this.getFather().getStartDate() == null) {
-            this.getFather().setStartDate(now);
-        }
+  public void accept(Visitor v) {
+    v.visitTask(this);
+  }
 
-        this.intervals.add(interval);
+  public List<Interval> getIntervals() {
+    return this.intervals;
+  }
+
+  public Interval getIntervalPos(int pos) {
+    return this.intervals.get(pos);
+  }
+
+  public void updateDates() {
+    this.elapsedTime = Duration.ZERO;
+
+    for (Interval interval : this.intervals) {
+      LocalDateTime finalDate = interval.getEnd();
+      Duration duration = interval.getElapsedTime();
+
+      if (finalDate != null)
+          this.setFinalDate(finalDate);
+
+      this.elapsedTime = this.elapsedTime.plus(duration);
     }
+    this.getFather().updateElapsedTime();
+  }
+  //This method updates the initalDate, finalDate and durationTime attributes iterating every son it has and looking for the initial and final Dates of the intervals.
+  //It also notifies its father about this change, if exists.
 
-    public void stop() {
-        LocalDateTime now = LocalDateTime.now();
-        this.setFinalDate(now);
-        this.updateDates();
-        //this.getFather().setFinalDate(now);
-    }
-
-    public void accept(Visitor v) {
-        v.visitTask(this);
-    }
-
-    public List<Interval> getIntervals() {
-        return this.intervals;
-    }
-
-    public Interval getIntervalPos(int pos) {return this.intervals.get(pos); }
-
-    @Override
-    public void updateDates() {
-        this.elapsedTime = Duration.ZERO;
-        Duration time = Duration.ZERO;
-        for (Interval interval : intervals) {
-            LocalDateTime initialDate = interval.getStart();
-            LocalDateTime finalDate = interval.getEnd();
-            Duration duration = interval.getElapsedTime();
-
-            if (initialDate != null) {
-                if (interval.getStart() == null || initialDate.compareTo(interval.getStart()) < 0)
-                    this.setStartDate(initialDate);
-            }
-            if (finalDate != null) {
-                if (interval.getEnd() == null || finalDate.compareTo(interval.getEnd()) > 0)
-                    this.setFinalDate(finalDate);
-            }
-            this.elapsedTime = this.elapsedTime.plus(duration);
-        }
-        notifyFather();
-    }
-    //This method updates the initalDate, finalDate and durationTime attributes iterating every son it has and looking for the initial and final Dates of the intervals.
-    //It also notifies its father about this change, if exists.
-
-    public String toString() {
-        return this.getName() + "          child of " + this.getFather().getName() + "      " + this.getStartDate() + "       " + this.getFinalDate() + "     " + this.getElapsedTime().getSeconds();
-    }
+  public String toString() {
+    return this.getName() + "          child of " + this.getFather().getName() + "      " + this.getStartDate() + "       " + this.getFinalDate() + "     " + this.getElapsedTime().getSeconds();
+  }
+  //This method is used to print the information of a Task.
 }
