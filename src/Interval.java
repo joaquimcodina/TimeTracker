a6937991352e3b3f7de08f74cc83c1e393314749
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-31 21:29:12 +0100

    Update Interval.java

--- src/Interval.java
+++ src/Interval.java
@@ -9,33 +9,51 @@ public class Interval implements Observer {
   private LocalDateTime end;
   private Task father;
   private Duration elapsedTime = Duration.ZERO;
-  public Interval(LocalDateTime now, Task father) {
-    this.start = now;
+
+  public Interval(Task father) {
+    this.start = LocalDateTime.now();
     this.father = father;
   }
+
   public void update(Observable o, Object arg) {
-    this.end = ClockTimer.getInstance().getNow();
-    this.elapsedTime = Duration.ofSeconds(Duration.between(start,end).toSeconds());
-    this.father.updateElapsedTime(getEnd());
-    //this.father.updateElapsedTime(getElapsedTime());
+    //this.end = ClockTimer.getInstance().getNow();
+    //this.elapsedTime = Duration.ofSeconds(Duration.between(this.start,this.end).toSeconds());
   }
 
-  public void stopClock() { ClockTimer.getInstance().deleteObserver(this); }
-  public LocalDateTime getStart(){ return this.start; }
-  public LocalDateTime getEnd(){ return this.end; }
+  public void update() {
+    this.end = ClockTimer.getInstance().getNow();
+    this.elapsedTime = Duration.ofSeconds(Duration.between(this.start, this.end).toSeconds());
+  }
+  //This method is called when an Interval is ended. It updates its information such as the elapsedTime and the endDate.
+
+  public void stopClock() {
+    ClockTimer.getInstance().deleteObserver(this);
+  }
+
+  public LocalDateTime getStart() {
+    return this.start;
+  }
+
+  public LocalDateTime getEnd() {
+    return this.end;
+  }
+
   public Task getFather() {
     return father;
   }
-  public Duration getElapsedTime(){ return this.elapsedTime; }
+
+  public Duration getElapsedTime() {
+    return this.elapsedTime;
+  }
+
+  public void setEndDate(LocalDateTime end) {
+    this.end = end;
+  }
 
   @Override
   public String toString() {
-    if(getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).equals("null"))
-      return "Interval         child of " + getFather().getName() + "      " + getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-          + "       null" + getElapsedTime().getSeconds();
-
-    else
-      return "Interval         child of " + getFather().getName() + "      " +getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-      + "       " + getEnd().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "     " + getElapsedTime().getSeconds();
+      return "Interval         child of " + getFather().getName() + "      " + getStart() + "      "
+          + getEnd() + "      " + getElapsedTime().getSeconds();
   }
+  //This method is used to print the information of an Interval.
 }
