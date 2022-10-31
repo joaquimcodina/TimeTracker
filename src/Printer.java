8d73a8630a9ec7b20a0d3efd3e2e7b9ce75725fa
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-31 21:29:31 +0100

    Update Printer.java

--- src/Printer.java
+++ src/Printer.java
@@ -4,25 +4,32 @@
 //
 
 public class Printer implements Visitor {
-    private Project project;
+  private Project project;
 
-    public Printer(Project project) {
-        this.project = project;
-    }
+  public Printer(Project project) {
+    this.project = project;
+  }
 
-    public void visitProject(Project project) {
-        System.out.println("Project " + project);
-        for (Component component : project.getComponentList()) {
-            component.accept(this);
-        }
+  public void visitProject(Project project) {
+    System.out.println("Project " + project);
+    for (Component component : project.getComponentList()) {
+      component.accept(this);
     }
+    if (project.getName() == "root")
+      System.out.println();
+  }
+  //this method prints the information of a project and its sub-components.
 
-    public void visitTask(Task task) {
-        System.out.println("Task " + task);
-        if (task.getIntervals().size() != 0) {
-            for (int pos = 0; pos < task.getIntervals().size(); pos++){
-                System.out.println("   " + task.getIntervalPos(pos));
-            }
-        }
+  public void visitTask(Task task) {
+    System.out.println("Task " + task);
+    if (task.getIntervals().size() != 0) {
+      for (int pos = 0; pos < task.getIntervals().size(); pos++){
+        System.out.println("   " + task.getIntervalPos(pos));
+      }
     }
+  }
+  //this method prints the information of a tasj and its intervals.
+
+  public void visitInterval(Interval interval) {
+  }
 }
