95da4948a1eba031350231a82dda88c2d4881369
Miguel <93439131+MiguelVallina2002@users.noreply.github.com>
2022-10-31 21:31:29 +0100

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,126 +1,168 @@
 import java.io.*;
+import java.time.Clock;
 import java.time.LocalDateTime;
-import org.json.*;
+//import org.json.*;
 
 public class User {
 
-    public static void jsonSafe(Project project, File file) throws IOException {
-      //JSON PART
-      JSONArray jsonArray = new JSONArray();
-      JSONObject obj;
-      FileWriter fileData = new FileWriter(file, false);
+  /*public static void jsonSafe(Project project, File file) throws IOException {
+    //JSON PART
+    JSONArray jsonArray = new JSONArray();
+    JSONObject obj;
+    FileWriter fileData = new FileWriter(file, false);
 
-      //JSONObject dels projectes, tasques filles
-      for(int i=0 ; i < project.getComponentList().size(); i++){
-        obj = new JSONObject();
-        obj.put("name", project.getComponentList().get(i).getName());
-        if(project.getComponentList().get(i).getFather() == null)
-          obj.put("father", "null");
-        else
-          obj.put("father", project.getComponentList().get(i).getFather().getName());
+    //JSONObject dels projectes, tasques filles
+    for(int i=0 ; i < project.getComponentList().size(); i++){
+      obj = new JSONObject();
+      obj.put("name", project.getComponentList().get(i).getName());
+      if(project.getComponentList().get(i).getFather() == null)
+        obj.put("father", "null");
+      else
+        obj.put("father", project.getComponentList().get(i).getFather().getName());
 
-        obj.put("type", project.getComponentList().get(i).getClass());
-        obj.put("start_date", project.getComponentList().get(i).getStartDate());
-        obj.put("final_date", project.getComponentList().get(i).getFinalDate());
-        obj.put("elapsed_time", project.getComponentList().get(i).getElapsedTime().toSeconds());
-        jsonArray.put(obj);
-      }
-      obj = new JSONObject(); //father object
-      obj.put("name", project.getName());
-      obj.put("father", "null");
-      obj.put("type", project.getClass()); //tipus projecte, tasca o interval
-      obj.put("start_date", project.getStartDate());
-      obj.put("final_date", project.getFinalDate());
-      obj.put("elapsed_time", project.getElapsedTime().toSeconds());
-      obj.put("children", jsonArray); //add list father
-
-      fileData.write(obj.toString());
-      fileData.flush();
-      fileData.close();
+      obj.put("type", project.getComponentList().get(i).getClass());
+      obj.put("start_date", project.getComponentList().get(i).getStartDate());
+      obj.put("final_date", project.getComponentList().get(i).getFinalDate());
+      obj.put("elapsed_time", project.getComponentList().get(i).getElapsedTime().toSeconds());
+      jsonArray.put(obj);
     }
+    obj = new JSONObject(); //father object
+    obj.put("name", project.getName());
+    obj.put("father", "null");
+    obj.put("type", project.getClass()); //tipus projecte, tasca o interval
+    obj.put("start_date", project.getStartDate());
+    obj.put("final_date", project.getFinalDate());
+    obj.put("elapsed_time", project.getElapsedTime().toSeconds());
+    obj.put("children", jsonArray); //add list father
 
-    public static Project simpleTest() throws InterruptedException {
-        Project root = new Project("root");
-        Project softwareDesign = new Project("Software Design", root);
-        new Project("Software Testing", root);
-        new Project("Database", root);
-        Task transportation = new Task("Transportation", root);
-        Project problems = new Project("Problems", softwareDesign);
-        Project projectTimeTracker = new Project("Project Time Tracker", softwareDesign);
-        Task task1 = new Task("First List", problems);
-        Task task2 = new Task("Second List", problems);
-        new Task("Read Handout", projectTimeTracker);
-        new Task("First Milestone", projectTimeTracker);
-        //Printer printer = new Printer(root);
-        //LocalDateTime now = LocalDateTime.now();
-        //System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now));
-        //root.accept(printer);
+    fileData.write(obj.toString());
+    fileData.flush();
+    fileData.close();
+  }*/
 
-        Printer printer = new Printer(root);
-        transportation.start(LocalDateTime.now());
-        Thread.sleep(4000L);
-        transportation.stop();
-        root.accept(printer);
-        Thread.sleep(2000L);
-        System.out.println("\n");
+  public static Project testA() throws InterruptedException {
 
-        task1.start(LocalDateTime.now());
-        Thread.sleep(6000L);
-        task2.start(LocalDateTime.now());
-        Thread.sleep(4000L);
-        task1.stop();
-        root.accept(printer);
-        Thread.sleep(2000L);
-        task2.stop();
-        root.accept(printer);
-        Thread.sleep(2000L);
-        System.out.println("\n");
+    System.out.println("\nBegin of Test A\n");
 
-        transportation.start(LocalDateTime.now());
-        Thread.sleep(4000L);
-        transportation.stop();
-        root.accept(printer);
-        System.out.println("\n");
+    Project root = new Project("root");
 
-        /*task1.start(now);
-        Thread.sleep(2000L);
-        task1.stop();
-        System.out.println("\n");
-        now = LocalDateTime.now();
-        System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now));
-        root.accept(printer);
-        Thread.sleep(2000L);
-        task1.stop();
-        System.out.println("\n");
-        now = LocalDateTime.now();
-        System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now));
-        root.accept(printer);
-        task2.start(now);
-        Thread.sleep(2000L);
-        task1.stop();
-        task2.stop();
-        System.out.println("\n");
-        now = LocalDateTime.now();
-        System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now));
-        root.accept(printer);*/
-        System.out.println("\n");
-        ClockTimer.getInstance().stopClock();
-        return root;
-    }
+    Project softwareDesign = new Project("Software Design", root);
+    Project st = new Project("Software Testing", root);
+    Project db = new Project("Database", root);
+    Task transportation = new Task("Transportation", root);
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-      //read file
-      String resourceName = "data/data.json";
-      File file = new File(resourceName);
-      //JSONArray jsonArray = new JSONArray();
-      //JSONObject obj;
-      //if (file.exists()) {
+    Project problems = new Project("Problems", softwareDesign);
+    Project projectTimeTracker = new Project("Project Time Tracker", softwareDesign);
+
+    Task fl = new Task("First List", problems);
+    Task sl = new Task("Second List", problems);
+
+    Task rh = new Task("Read Handout", projectTimeTracker);
+    Task fm = new Task("First Milestone", projectTimeTracker);
+
+    System.out.println("\nEnd of Test A\n");
+
+    return root;
+  }
+
+  public static void testB() throws InterruptedException {
+    Project root = new Project("root");
+
+    Project softwareDesign = new Project("Software Design", root);
+    Project st = new Project("Software Testing", root);
+    Project db = new Project("Database", root);
+    Task transportation = new Task("Transportation", root);
+
+    Project problems = new Project("Problems", softwareDesign);
+    Project projectTimeTracker = new Project("Project Time Tracker", softwareDesign);
+
+    Task task1 = new Task("First List", problems);
+    Task task2 = new Task("Second List", problems);
+
+    Task rh = new Task("Read Handout", projectTimeTracker);
+    Task fm = new Task("First Milestone", projectTimeTracker);
+
+    Printer printer = new Printer(root);
+
+    System.out.println("\nBegin of Test B\n");
+
+    //Point 1:
+    ClockTimer.getInstance().startClock();
+    Thread.sleep(1500L);
+    root.accept(printer);
+    //Point 1:
+
+    //Point 2:
+    transportation.start();
+    Thread.sleep(6000L);
+    transportation.stop();
+    root.accept(printer);
+    //Point 2:
+
+    //Point 3:
+    Thread.sleep(2000L);
+    root.accept(printer);
+    System.out.println("\n");
+    //Point 3:
+
+    //Point 4:
+    task1.start();
+    Thread.sleep(6000L);
+    root.accept(printer);
+    //Point 4:
+
+    //Point 5:
+    task2.start();
+    Thread.sleep(4000L);
+    root.accept(printer);
+    //Point 5:
+
+    //Point 6:
+    task1.stop();
+    root.accept(printer);
+    //Point 6:
+
+    //Point 7:
+    Thread.sleep(2000L);
+    task2.stop();
+    root.accept(printer);
+    //Point 7:
+
+    //Point 8:
+    Thread.sleep(2000L);
+    root.accept(printer);
+    System.out.println("\n");
+    //Point 8:
+
+    //Point 9:
+    transportation.start();
+    Thread.sleep(4000L);
+    transportation.stop();
+    root.accept(printer);
+    System.out.println("\n");
+    //Point 9:
+
+    System.out.println("\n");
+    ClockTimer.getInstance().stopClock();
+
+    System.out.println("\nEnd of Test B\n");
+  }
+
+  public static void main(String[] args) throws InterruptedException, IOException {
+    //read file
+    //String resourceName = "data/data.json";
+    //File file = new File(resourceName);
+    //JSONArray jsonArray = new JSONArray();
+    //JSONObject obj;
+    //if (file.exists()) {
         /*InputStream is = Files.newInputStream(file.toPath());
         if (is.available() > 0) {
           JSONTokener tokener = new JSONTokener(is);
           obj = new JSONObject(tokener);
         }*/
-      //}
-      jsonSafe(simpleTest(), file);
-    }
+    //}
+    //jsonSafe(simpleTest(), file);
+    testA();  //nice and correct
+    testB(); //nice and correct
+  }
 }
