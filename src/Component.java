e12b05bcffcd185b2bb6020946c7c13534b2ffc8
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-17 17:26:02 +0100

    Checkstyle Component

--- src/Component.java
+++ src/Component.java
@@ -15,109 +15,128 @@ import java.util.List;
     @since 2022-11-06
  */
 public abstract class Component {
-    private String name;
-    protected Duration elapsedTime;
-    private LocalDateTime startDate;
-    private LocalDateTime finalDate;
-    private Project father;
+  private final String name;
+  protected Duration elapsedTime;
+  private LocalDateTime startDate;
+  private LocalDateTime finalDate;
+  private final Project father;
 
-    public Component(String name){
-        this.name = name;
-        this.father = null;
-        this.elapsedTime = Duration.ZERO;
-        this.startDate = null;
-        this.finalDate = null;
-    }
+  public Component(String name) {
+    this.name = name;
+    this.father = null;
+    this.elapsedTime = Duration.ZERO;
+    this.startDate = null;
+    this.finalDate = null;
+  }
 
-    public Component(String name, Project father){
-        this.name = name;
-        this.father = father;
-        this.elapsedTime = Duration.ZERO;
-        this.startDate = null;
-        this.finalDate = null;
-    }
+  public Component(String name, Project father) {
+    this.name = name;
+    this.father = father;
+    this.elapsedTime = Duration.ZERO;
+    this.startDate = null;
+    this.finalDate = null;
+  }
 
-    /*
-This Constructor is used by the ReadJSON.java class in order to rebuild the hierarchy. It will be not be
-able to the Users. It basically initializes every single attribute a Component has.
-
+  /*
+This Constructor is used by the ReadJSON.java class in order to rebuild the hierarchy.
+It will be not be able to the Users.
+It basically initializes every single attribute a Component has.
 @return Component
 */
-    public Component(String name, Project father, Duration elapsedTime, LocalDateTime startDate, LocalDateTime finalDate){
-        this.name = name;
-        this.father = father;
-        this.elapsedTime = elapsedTime;
-        this.startDate = startDate;
-        this.finalDate = finalDate;
-    }
+  public Component(String name, Project father, Duration elapsedTime,
+                   LocalDateTime startDate, LocalDateTime finalDate) {
+    this.name = name;
+    this.father = father;
+    this.elapsedTime = elapsedTime;
+    this.startDate = startDate;
+    this.finalDate = finalDate;
+  }
 
-    public abstract ArrayList<Component> getComponentList();
-    public abstract List<Interval> getIntervals();
+  public abstract ArrayList<Component> getComponentList();
 
-    public String getName() {
-        return name;
-    }
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-    public LocalDateTime getFinalDate() { return finalDate; } //inalDate
-    public Duration getElapsedTime() { return elapsedTime; }
-    public Project getFather() {
-        return this.father;
-    }
+  public abstract List<Interval> getIntervals();
 
-    protected void setElapsedTime(Duration elapsedTime) {
-        this.elapsedTime = elapsedTime;
-    }
+  public String getName() {
+    return name;
+  }
 
-    protected void sumElapsedTime(Duration elapsedTime) {
-        this.elapsedTime = this.elapsedTime.plus(elapsedTime);
-    }
+  public LocalDateTime getStartDate() {
+    return startDate;
+  }
 
-    /*
+  //initialDate
+  public LocalDateTime getFinalDate() {
+    return finalDate;
+  }
+
+  public Duration getElapsedTime() {
+    return elapsedTime;
+  }
+
+  public Project getFather() {
+    return this.father;
+  }
+
+  protected void setElapsedTime(Duration elapsedTime) {
+    this.elapsedTime = elapsedTime;
+  }
+
+  protected void sumElapsedTime(Duration elapsedTime) {
+    this.elapsedTime = this.elapsedTime.plus(elapsedTime);
+  }
+
+  /*
 This function updates its own startDate and expands recursively the inner changes to the
 upper nodes in the hierarchy, if exists.
 */
-    public void setStartDate(LocalDateTime startDate) {
-        if (this.getStartDate() == null)
-            this.startDate = startDate;
-        if (this.father != null)
-            father.setStartDate(startDate);
+  public void setStartDate(LocalDateTime startDate) {
+    if (this.getStartDate() == null) {
+      this.startDate = startDate;
     }
+    if (this.father != null) {
+      father.setStartDate(startDate);
+    }
+  }
 
-    /*
+  /*
 This function updates its own finalDate and expands recursively the inner changes to the
 upper nodes in the hierarchy, if exists.
 */
-    public void setFinalDate(LocalDateTime finalDate) {
-        if (this.finalDate == null || this.finalDate.compareTo(finalDate) < 0)
-            this.finalDate = finalDate;
-        if (father != null)
-            father.setFinalDate(finalDate);
+  public void setFinalDate(LocalDateTime finalDate) {
+    if (this.finalDate == null || this.finalDate.compareTo(finalDate) < 0) {
+      this.finalDate = finalDate;
+    }
+    if (father != null) {
+      father.setFinalDate(finalDate);
+    }
+  }
+
+  public LocalDateTime getActualDate() {
+    return LocalDateTime.now();
+  }
+
+  public Duration getActualElapsedTime() {
+    Duration duration = this.elapsedTime;
+
+    if (this.getIntervals() == null) {
+      return Duration.ofSeconds(Duration.between(this.startDate, LocalDateTime.now()).toSeconds());
     }
 
-    public LocalDateTime getActualDate(){
-        return LocalDateTime.now();
-    }
-
-    public Duration getActualElapsedTime() {
-        Duration duration = this.elapsedTime;
-        if(this.getIntervals() == null)
-            return Duration.ofSeconds(Duration.between(this.startDate, LocalDateTime.now()).toSeconds());
-
-        if (getIntervals() != null){
-            for (Interval inter : getIntervals()){
-                if (inter.getActiu())
-                    duration = duration.plus(inter.getActualElapsedTime());
-            }
+    if (getIntervals() != null) {
+      for (Interval inter : getIntervals()) {
+        if (inter.getActiu()) {
+          duration = duration.plus(inter.getActualElapsedTime());
         }
-
-        return Duration.ofSeconds(duration.toSeconds());
+      }
     }
 
-    abstract void accept(Visitor v);
+    return Duration.ofSeconds(duration.toSeconds());
+  }
 
-    public String getFatherName() {
-        return this.father.getName();
-    }
+  abstract void accept(Visitor v);
+
+  public String getFatherName() {
+    assert this.father != null;
+    return this.father.getName();
+  }
 }
