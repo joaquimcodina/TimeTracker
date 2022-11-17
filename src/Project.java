b9d595e3e71b7f720f44ae45afe5e6053838e38f
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-18 00:22:37 +0100

    Checkstyle Project

--- src/Project.java
+++ src/Project.java
@@ -2,7 +2,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -14,23 +13,24 @@ import java.util.List;
     @since 2022-11-06
  */
 public class Project extends Component {
-    private ArrayList<Component> componentList = new ArrayList<>();
+  private ArrayList<Component> componentList = new ArrayList<>();
 
-    /*
-    This Constructor calls to the superclass constructor (super statement), and finally we notify
-    the father (that cannot be null) of this object's creation in order to this object be in its decendents.
+  /*
+  This Constructor calls to the superclass constructor (super statement),
+  and finally we notify the father (that cannot be null) of
+  this object's creation in order to this object be in its descendents.
 
-    @param name : Must be a String. This will be the name of the task.
-    @param father : Must be a Project. This param must not be null.
+  @param name : Must be a String. This will be the name of the task.
+  @param father : Must be a Project. This param must not be null.
 
-    @return Project
- */
-    Project(String name, Project father) {
-        super(name, father);
-        father.addComponent(this);
-    }
+  @return Project
+*/
+  Project(String name, Project father) {
+    super(name, father);
+    father.addComponent(this);
+  }
 
-    /*
+  /*
 This Constructor calls to the superclass constructor (super statement), and, finally, forces the
 father to be null, which means that this node is the root (or one of the roots) of the hierarchy.
 
@@ -38,71 +38,86 @@ father to be null, which means that this node is the root (or one of the roots) 
 
 @return Project
 */
-    Project(String name) {
-        super(name);
-    }
+  Project(String name) {
+    super(name);
+  }
 
-    /*
-This Constructor is used by the ReadJSON.java class in order to rebuild the hierarchy. It will be not be
-able to the Users. It basically initializes every single attribute a Project (and Component) has.
+  /*
+This Constructor is used by the ReadJson.java class in order to rebuild the hierarchy.
+It will be not be able to the Users.
+It basically initializes every single attribute a Project (and Component) has.
 
 @return Project
 */
-    public Project(String name, Project father, Duration elapsedTime, LocalDateTime startDate, LocalDateTime finalDate){
-        super(name, father, elapsedTime, startDate, finalDate);
-        if (father != null)
-            father.addComponent(this);
+  public Project(String name, Project father, Duration elapsedTime,
+                 LocalDateTime startDate, LocalDateTime finalDate) {
+    super(name, father, elapsedTime, startDate, finalDate);
+    if (father != null) {
+      father.addComponent(this);
     }
+  }
 
-    /*
-    This method updates the elapsed time of this project and, if it has father, recursively through
-    the method "setElapsedTime()", it updates the elapsed time of every node above it connected to it.
+  /*
+  This method updates the elapsed time of this project and, if it has father, recursively through
+  the method "setElapsedTime()", it updates the elapsed time of every node above it connected to it.
 */
-    public void updateElapsedTime() {
-        this.setElapsedTime(Duration.ZERO);
+  public void updateElapsedTime() {
+    this.setElapsedTime(Duration.ZERO);
 
-        for (Component component : this.componentList)
-            this.sumElapsedTime(component.getElapsedTime());
-
-        if (this.getFather() != null)
-            this.getFather().updateElapsedTime();
-    }
-    protected void addComponent(Component comp) {
-        this.componentList.add(comp);
+    for (Component component : this.componentList) {
+      this.sumElapsedTime(component.getElapsedTime());
     }
 
-    @Override
-    public ArrayList<Component> getComponentList() {
-        return this.componentList;
+    if (this.getFather() != null) {
+      this.getFather().updateElapsedTime();
     }
+  }
 
-    @Override
-    public List<Interval> getIntervals() {
-        return null;
-    }
+  protected void addComponent(Component comp) {
+    this.componentList.add(comp);
+  }
 
-    public void accept(Visitor v) {
-        v.visitProject(this);
-    }
+  @Override
+  public ArrayList<Component> getComponentList() {
+    return this.componentList;
+  }
 
-    /*
-    This method is used to print the information of a Project into console.
+  @Override
+  public List<Interval> getIntervals() {
+    return null;
+  }
+
+  public void accept(Visitor v) {
+    v.visitProject(this);
+  }
+
+  /*
+  This method is used to print the information of a Project into console.
 */
-    public String toString() {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String stringToReturn = "";
-        if (this.getFather() == null)
-            stringToReturn = this.getName() + "\t\t\t\t\t\tchild of null\t\t\t";
-        else
-            stringToReturn = this.getName() + "\t\t\t\t\t\tchild of \t\t\t" + this.getFather().getName() + "\t\t\t";
-        if (this.getStartDate() == null)
-            stringToReturn += "null\t\t\t";
-        else
-            stringToReturn += this.getStartDate().format(format) + "\t\t\t";
-        if (this.getFinalDate() == null)
-            stringToReturn += "null\t\t\t" + this.getElapsedTime().getSeconds();
-        else
-            stringToReturn += this.getFinalDate().format(format) + "\t\t\t" + this.getElapsedTime().getSeconds();
-        return stringToReturn;
+  public String toString() {
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    String stringToReturn;
+    if (this.getFather() == null) {
+      stringToReturn = this.getName()
+          + "\t\t\t\t\t\tchild of null\t\t\t";
+    } else {
+      stringToReturn = this.getName()
+          + "\t\t\t\t\t\tchild of \t\t\t"
+          + this.getFather().getName() + "\t\t\t";
     }
+    if (this.getStartDate() == null) {
+      stringToReturn += "null\t\t\t";
+    } else {
+      stringToReturn += this.getStartDate().format(format) + "\t\t\t";
+    }
+    if (this.getFinalDate() == null) {
+      stringToReturn += "null\t\t\t" + this.getElapsedTime().getSeconds();
+    } else {
+      stringToReturn
+          += this.getFinalDate().format(format)
+          + "\t\t\t"
+          + this.getElapsedTime().getSeconds();
+    }
+    return stringToReturn;
+  }
 }
