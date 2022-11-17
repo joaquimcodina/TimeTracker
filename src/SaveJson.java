1f7e330961cb91fec352bd7887d257e242a46d81
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-17 23:45:53 +0100

    Checkstyle SaveJson

---
+++ src/SaveJson.java
@@ -0,0 +1,119 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+/*
+    This class implements the Visitor (Visitor.java) class,
+    which implements the Visitor Design Pattern.
+    It is used to create a file and save the Time Tracker
+    application's data into a .JSON file, which
+    will be called "./data/data.json".
+
+    @version 2.0
+    @since 2022-11-06
+ */
+
+public class SaveJson implements Visitor {
+  private JSONArray jsonArray = new JSONArray();
+  private JSONObject obj;
+  private FileWriter fileData;
+
+  /*
+  This constructor directly initializes the saving process
+  of the application into a .JSON file by calling other
+  methods of this class.
+
+  @param root : must be a Component object.
+
+  @return SaveJSON
+
+  @usage: new SaveJSON(Component);
+*/
+  public SaveJson(Component root) throws IOException {
+    System.out.println("Saving data in a .json file...");
+    String path = "data/data.json";
+    File file = new File(path);
+    this.fileData = new FileWriter(file, false);
+    if (root != null) {
+      root.accept(this); //recursive
+    }
+    try {
+      this.obj = new JSONObject();
+      this.obj.put("components", jsonArray);
+      this.fileData.write(this.obj.toString());
+      this.fileData.close();
+
+    } catch (IOException err) {
+      throw new RuntimeException(err);
+    }
+    System.out.println(".json file successfully created!");
+  }
+
+  @Override
+  public void visitProject(Project var1) {
+    inputData(var1);
+    for (Component component : var1.getComponentList()) {
+      component.accept(this);
+    }
+  }
+
+  /*
+  This method puts the information of a hierarchy's node into a JSON object, which is put into a
+  JSON array, which will be written into the file.
+*/
+  private void inputData(Component var1) {
+    JSONObject obj = new JSONObject();
+    obj.put("name", var1.getName());
+    if (var1.getFather() == null) {
+      obj.put("father", "null");
+    } else {
+      obj.put("father", var1.getFatherName());
+    }
+    obj.put("type", var1.getClass().getName());
+
+    if (var1.getStartDate() == null) {
+      obj.put("start_date", "null");
+    } else {
+      obj.put("start_date", var1.getStartDate());
+    }
+    
+    if (var1.getFinalDate() == null) {
+      obj.put("final_date", "null");
+    } else {
+      obj.put("final_date", var1.getActualDate());
+    }
+
+    obj.put("elapsed_time", var1.getElapsedTime());
+
+    this.jsonArray.put(obj);
+  }
+  //This method saves in a JSON Object the information to be written in a .json file.
+
+  @Override
+  public void visitTask(Task var1) {
+    inputData(var1);
+    for (Interval interval : var1.getIntervals()) {
+      interval.accept(this);
+    }
+  }
+
+  public void visitInterval(Interval interval) {
+    JSONObject obj = new JSONObject();
+    obj.put("father", interval.getFather().getName()); //don't talk to strangers?
+    if (interval.getStart() == null) {
+      obj.put("start_date", "null");
+    } else {
+      obj.put("start_date", interval.getStart());
+    }
+    
+    if (interval.getEnd() == null) {
+      obj.put("final_date", "null");
+    } else {
+      obj.put("final_date", interval.getEnd());
+    }
+    obj.put("elapsed_time", interval.getElapsedTime());
+    obj.put("type", interval.getClass().getName());
+    this.jsonArray.put(obj);
+  }
+}
