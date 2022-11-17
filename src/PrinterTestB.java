96ab24df7e67f0143af8a1d7b053441b6f44f3c0
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-17 21:52:05 +0100

    Checkstyle PrinterTestB

--- src/PrinterTestB.java
+++ src/PrinterTestB.java
@@ -9,50 +9,60 @@ import java.util.Observer;
     @version 2.0
     @since 2022-11-06
  */
-public class PrinterTestB implements Visitor , Observer{
+public class PrinterTestB implements Visitor, Observer {
 
-    private Interval interval;
+  private Interval interval;
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public PrinterTestB(){
+  public PrinterTestB() {
 
+  }
+
+  @Override
+  public void visitProject(Project project) {
+
+    System.out.println("activity: \t\t" + project.getName() + "\t\t\t\t"
+        + project.getStartDate().format(formatter)
+        + "\t\t\t" + project.getActualDate().format(formatter) + "\t\t\t"
+        + project.getActualElapsedTime().toSeconds());
+
+    if (project.getFather() != null) {
+      project.getFather().accept(this);
+    } else {
+      System.out.println();
+    }
+  }
+
+  @Override
+  public void visitTask(Task task) {
+    System.out.println("activity: \t\t" + task.getName()
+        + "\t\t\t" + task.getStartDate().format(formatter)
+        + "\t\t\t" + task.getActualDate().format(formatter)
+        + "\t\t\t" + task.getActualElapsedTime().toSeconds());
+    if (task.getFather() != null) {
+      task.getFather().accept(this);
+    }
+  }
+
+  @Override
+  public void visitInterval(Interval interval) {
+    if (interval.getFather().isStopped()) {
+      System.out.print("");
     }
 
-    @Override
-    public void visitProject(Project project) {
+    System.out.println("interval:  \t\t\t\t\t\t\t" + interval.getStart().format(formatter)
+        + "\t\t\t" + interval.getActualDate().format(formatter) + "\t\t\t\t"
+        + interval.getActualElapsedTime().toSeconds());
 
-        System.out.println("activity: \t\t" + project.getName() +"\t\t\t\t" + project.getStartDate().format(formatter)+
-                "\t\t\t" + project.getActualDate().format(formatter) + "\t\t\t" + project.getActualElapsedTime().toSeconds());
-        if (project.getFather() != null)
-            project.getFather().accept(this);
-        else
-            System.out.println();
-    }
+    interval.getFather().accept(this);
+  }
 
-    @Override
-    public void visitTask(Task task) {
-        System.out.println("activity: \t\t" + task.getName() +"\t\t\t" + task.getStartDate().format(formatter)+
-                "\t\t\t" + task.getActualDate().format(formatter) + "\t\t\t" + task.getActualElapsedTime().toSeconds());
-        if (task.getFather() != null)
-            task.getFather().accept(this);
-    }
-
-    @Override
-    public void visitInterval(Interval interval){
-        if(interval.getFather().isStopped())
-            return;
-
-        System.out.println("interval:  \t\t\t\t\t\t\t" + interval.getStart().format(formatter)+
-                "\t\t\t" + interval.getActualDate().format(formatter) + "\t\t\t\t" + interval.getActualElapsedTime().toSeconds());
-
-        interval.getFather().accept(this);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) { //IMPRIMIR ULTIM INTERVAL ACTIU
-        interval = ClockTimer.getInstance().getObservers().get(ClockTimer.getInstance().getObservers().size()-1);
-        interval.accept(this);
-    }
+  @Override
+  public void update(Observable o, Object arg) { //print latest active interval
+    interval = ClockTimer.getInstance().getObservers()
+        .get(ClockTimer.getInstance().getObservers().size() - 1);
+    interval.accept(this);
+  }
 
 }
