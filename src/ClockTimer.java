89225290e447a64a659971879bb44c7d6ee8287e
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-11-17 16:56:33 +0100

    Checkstyle ClockTimer

--- src/ClockTimer.java
+++ src/ClockTimer.java
@@ -1,7 +1,10 @@
-import com.fasterxml.jackson.databind.ser.std.ObjectArraySerializer;
-
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /*
     This class implements the Observable Design Pattern. It basically has a Clock that will be
@@ -12,12 +15,12 @@ import java.util.*;
  */
 
 public class ClockTimer extends Observable {
-  private Timer timer;
-  private List<Interval> observers = new LinkedList<Interval>();
+  private final Timer timer;
+  private final List<Interval> observers = new LinkedList<>();
   private LocalDateTime now;
   private static final ClockTimer instance = new ClockTimer();
-  private static final double freq = 2.75;
-  private ClockTimer(){
+
+  private ClockTimer() {
     this.timer = new Timer();
     startClock();
   }
@@ -25,9 +28,9 @@ public class ClockTimer extends Observable {
   /*
 This method starts the clock of the ClockTimer's class.
 */
-  public void startClock(){
+  public void startClock() {
     // Get current date time init
-    TimerTask cycle_task = new TimerTask() {
+    TimerTask cycleTask = new TimerTask() {
       @Override
       public void run() {
         now = LocalDateTime.now();
@@ -36,29 +39,37 @@ This method starts the clock of the ClockTimer's class.
 
       }
     };
-    this.timer.scheduleAtFixedRate(cycle_task, 0, (long) (2000.0));
+    this.timer.scheduleAtFixedRate(cycleTask, 0, (long) (2000.0));
   }
-  public void stopClock(){
+
+  public void stopClock() {
     this.timer.cancel();
   }
-  public static ClockTimer getInstance() { return instance; }
-  public static double getFreq() { return freq; }
-  public LocalDateTime getNow() { return now; }
+
+  public static ClockTimer getInstance() {
+    return instance;
+  }
+
+  public LocalDateTime getNow() {
+    return now;
+  }
 
   @Override
   public synchronized void addObserver(Observer o) {
     super.addObserver(o);
   }
 
-  public void addInterval(Interval inter){
+  public void addInterval(Interval inter) {
     observers.add(inter);
   }
-  public List<Interval> getObservers(){
+
+  public List<Interval> getObservers() {
     return observers;
   }
+
   @Override
   public void notifyObservers() {
-    super.notifyObservers();
+    super.notifyAll();
   }
 
 }
