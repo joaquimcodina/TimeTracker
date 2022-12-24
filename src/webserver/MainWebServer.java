194f904d873870fab8129978dcfb68f859a68435
joaquimcodina <joaquim.codina.espin@gmail.com>
2022-12-24 16:50:12 +0100

    Canviar IDs projectes i tasques

--- src/webserver/MainWebServer.java
+++ src/webserver/MainWebServer.java
@@ -18,20 +18,20 @@ public class MainWebServer {
   }
 
   public static Component makeTree() {
-    Project root = new Project(1, "root");
-    Project softwareDesign = new Project(2, "Software Design", root);
-    new Project(3, "Software Testing", root);
-    new Project(4, "Databases", root);
-    new Task(5, "Transportation", root);
+    Project root = new Project(0, "root");
+    Project softwareDesign = new Project(1, "Software Design", root);
+    new Project(2, "Software Testing", root);
+    new Project(3, "Databases", root);
+    new Task(4, "Transportation", root);
 
-    Project problems = new Project(6, "Problems", softwareDesign);
-    Project projectTimeTracker = new Project(7, "Project Time Tracker", softwareDesign);
+    Project problems = new Project(5, "Problems", softwareDesign);
+    Project projectTimeTracker = new Project(6, "Project Time Tracker", softwareDesign);
 
-    new Task(8, "First List", problems);
-    new Task(9, "Second List", problems);
+    new Task(7, "First List", problems);
+    new Task(8, "Second List", problems);
 
-    new Task(10, "Read Handout", projectTimeTracker);
-    new Task(11, "First Milestone", projectTimeTracker);
+    new Task(9, "Read Handout", projectTimeTracker);
+    new Task(10, "First Milestone", projectTimeTracker);
 
     return root;
   }
